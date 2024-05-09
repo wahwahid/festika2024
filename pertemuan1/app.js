866b956a24ea1e94bdd2227c5a5e0b7e550027f4
wahwahid <the@wahwahid.com>
2024-05-09 11:08:20 +0700

    using dotenv config

--- pertemuan1/app.js
+++ pertemuan1/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const indexRouter = require('./routes/index')
+
+res = dotenv.config()
 
 const app = express()
 app.use(indexRouter)
 
-const port = 3000
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Aplikasi menika siap wonten ing port: ${port}`)
-})
+}) 
