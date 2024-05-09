9db94499f26c111944b9229693c8204165fdcdcc
wahwahid <the@wahwahid.com>
2024-05-09 22:18:03 +0700

    multipart form

--- pertemuan1/routes/ngebody.js
+++ pertemuan1/routes/ngebody.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const multer = require('multer')
 const router = express.Router()
 
+const upload = multer()
+
 router.post('/basic', (req, res) => {
+    res.json(req.body)
+})
+
+router.post('/advanced', upload.none(), (req, res) => {
     res.json(req.body)
 })
 
