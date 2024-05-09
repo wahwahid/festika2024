7a9d07d07863c5ed750f1261f79089aeb8a44bf5
wahwahid <the@wahwahid.com>
2024-05-09 22:33:42 +0700

    input single file

--- pertemuan1/routes/ngebody.js
+++ pertemuan1/routes/ngebody.js
@@ -12,4 +12,22 @@ router.post('/advanced', upload.none(), (req, res) => {
     res.json(req.body)
 })
 
+router.post('/file', upload.single('file'), (req, res) => {
+    const { originalname, mimetype } = req.file
+    let data = []
+    if (mimetype == 'text/csv') {
+        const lines = req.file.buffer.toString().split('\n')
+        const headers = lines[0].trim().split(',')
+        for (let i = 0; i < lines.length; i++) {
+            const row = lines[i].trim().split(',')
+            let item = {}
+            headers.forEach((header, col) => {
+                item[header] = row[col]
+            })
+            data.push(item)
+        }
+    }
+    res.json({ originalname, mimetype, data })
+})
+
 module.exports = router
