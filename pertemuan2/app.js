dd721adddc4f2c2f16bc7a5a8e81ed697013d996
wahwahid <the@wahwahid.com>
2024-05-15 23:25:53 +0700

    http log middleware

--- pertemuan2/app.js
+++ pertemuan2/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const hbs = require('express-hbs')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const path = require('path')
 const mw = require('./middlewares/index')
 const indexRouter = require('./routes/index')
-const serviceLog = require('./service/log')
+const { log, SetLog } = require('./service/log')
 
 dotenv.config()
-serviceLog.SetLog({
+SetLog({
     env: process.env.NODE_ENV || "",
     level: process.env.LOG_LEVEL || "",
     destination: process.env.LOG_DIR || "",
@@ -24,6 +25,11 @@ app.engine('html', hbs.express4({
   }
 }))
 // general routes
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]', {
+  stream: {
+    write: (message) => log().http(message.trim()),
+  },
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(indexRouter)
@@ -32,5 +38,5 @@ app.use(mw.error.unhandledError)
 
 const port = process.env.PORT
 app.listen(port, () => {
-    console.log(`Aplikasi menika siap wonten ing port: ${port}`)
+    log().info(`Aplikasi menika siap wonten ing port: ${port}`)
 })
